cdd6a0ea80673c140f5e4f02e6fcad3b116be38c
José Pedro <97629866+sejodrope@users.noreply.github.com>
2024-09-10 19:52:22 -0300

    Commit inicial

---
+++ src/main/java/br/agrimedi/agrimediweb/AgrimediwebApplication.java
@@ -0,0 +1,13 @@
+package br.agrimedi.agrimediweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrimediwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrimediwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/agrimedi/agrimediweb/AgrimediwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.agrimedi.agrimediweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrimediwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
